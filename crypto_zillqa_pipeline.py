a7a52c78cc4fd2e2fac2ae5a8616b6c6f1eadf2b
Hazperera <heperera826@gmail.com>
2024-02-07 21:31:33 +0000

    update commands.txt

--- crypto_zillqa_pipeline.py
+++ crypto_zillqa_pipeline.py
@@ -1,22 +1,22 @@
-from google.cloud import bigquery
+# from google.cloud import bigquery
 
-client = bigquery.Client()
+# client = bigquery.Client()
 
-# Perform a query - summary of daily transaction volumes
+# # Perform a query - summary of daily transaction volumes
 
-QUERY = (
-    'CREATE TABLE datafella-workbook.dbt_hp.crypto_daily_transaction_summary AS'
-    'SELECT DATE(block_timestamp) as transaction_date,' 
-    'COUNT(id) as total_transactions,'
-    'SUM(amount) as total_amount'
-    'FROM `public-data-finance.crypto_zilliqa.transactions`'
-    'GROUP BY transaction_date'
-    )
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
+# QUERY = (
+#     'CREATE TABLE datafella-workbook.dbt_hp.crypto_daily_transaction_summary AS'
+#     'SELECT DATE(block_timestamp) as transaction_date,' 
+#     'COUNT(id) as total_transactions,'
+#     'SUM(amount) as total_amount'
+#     'FROM `public-data-finance.crypto_zilliqa.transactions`'
+#     'GROUP BY transaction_date'
+#     )
+# query_job = client.query(QUERY)  # API request
+# rows = query_job.result()  # Waits for query to finish
 
-for row in rows:
-    print(row.name)
+# for row in rows:
+#     print(row.name)
 
 
 

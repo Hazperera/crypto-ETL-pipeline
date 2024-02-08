6736816bcee0aed815169093f7d22f9d90a0fa3b
Hazperera <heperera826@gmail.com>
2024-02-08 21:45:50 +0000

    update bq-to-parquet.py file

--- bq-to-parquet.py
+++ bq-to-parquet.py
@@ -1,17 +1,22 @@
 from google.cloud import bigquery
+import pandas as pd
 
 # Initialize BigQuery client
 client = bigquery.Client()
 
 # Perform a query
-QUERY = (
-    'SELECT * FROM `public-data-finance.crypto_zilliqa.transactions` '
-    'LIMIT 10')
-query_job = client.query(QUERY)  # API request
+first_query = (
+    'SELECT * '
+    'FROM `public-data-finance.crypto_zilliqa.transactions` '
+    'LIMIT 1')
+query_job = client.query(first_query)  # API request
 rows = query_job.result()  # Waits for query to finish
 
-for row in rows:
-    print(row)
+# Convert query result to DataFrame
+df = pd.DataFrame([dict(row) for row in rows])
+
+# Write DataFrame to Parquet file
+df.to_parquet('crypto_zilliqa_transactions.parquet', index=False)
 
 
 

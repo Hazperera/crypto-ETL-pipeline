99572357cdbda8f92fc811a09f6a1915a297988a
Hazperera <heperera826@gmail.com>
2024-06-08 23:14:10 +0100

    add models

--- zilliqa_project/models/example/key_metrics.sql
+++ zilliqa_project/models/example/key_metrics.sql
@@ -1,2 +1,12 @@
 
+-- Block Creation Rate
 
+SELECT
+  DATE_TRUNC(timestamp, DAY) AS day,
+  COUNT(*) AS blocks_created
+FROM
+  `bigquery-public-data.crypto_zilliqa.ds_blocks`
+GROUP BY
+  day
+ORDER BY
+  day;

eefc7f6268e9bb89972b84733b9457201d1f7691
Hazperera <heperera826@gmail.com>
2024-06-09 15:27:03 +0100

    add changes to fct_block_creation model

--- zilliqa_project/models/fct_block_creation.sql
+++ zilliqa_project/models/fct_block_creation.sql
@@ -20,12 +20,14 @@ tx_blocks_metrics AS (
 )
 
 SELECT
-  d.date,
+  COALESCE(ds.date, tx.date) AS date,
   COALESCE(ds.blocks_created_ds, 0) AS blocks_created_ds,
   COALESCE(tx.blocks_created_tx, 0) AS blocks_created_tx
 FROM
-  {{ ref('dim_date') }} d
-  LEFT JOIN ds_blocks_metrics ds ON d.date = ds.date
-  LEFT JOIN tx_blocks_metrics tx ON d.date = tx.date
+  ds_blocks_metrics ds
+FULL OUTER JOIN
+  tx_blocks_metrics tx
+ON
+  ds.date = tx.date
 ORDER BY
-  d.date
+  date
